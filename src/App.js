a8708800323ba3e93730ea0f01b6ed30bf8c0e3d
ginaperez <gmperez92@gmail.com>
2019-10-31 15:52:05 -0700

    gina perez changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.png/';
+import React from 'react';
+import { Switch, NavLink, Route } from 'react-router-dom';
+import logo from './logo.png'
 import './App.css';
+import AuthComponent from './components/AuthComponent';
 
 class App extends React.Component {
   render() {
-  return <div className="App"></div>
+  return <div className="App">
+          <header>
+            {/* inner double container */}
+            <div className='logo'>
+              {/* logo container */}
+              <img src={logo} />
+            </div>
+            {/* nav container */}
+            <nav>
+            <NavLink activeClassName="active" exact to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/store">Store</NavLink>
+            <NavLink activeClassName="active" to="/profile">Profile</NavLink>
+            </nav>
+          </header>
+          <Switch>
+            <Route exact path="/" component={AuthComponent} />
+            <Route exact path="/store" render={() => {
+              return <div>Store</div> 
+            }}/>
+            <Route exact path="/profile" render={() => {
+              return <div>Profile</div> 
+            }}/>
+            <Route exact path="*" render={() => {
+              return <div>????</div> 
+            }}/>
+          </Switch>
+         </div>
   }
 }
 
---
+++ src/components/AuthComponent.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setUser } from '../ducks/reducer';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+
+class AuthComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            username: "",
+            register: true
+        };
+        this.register = this.register.bind(this);
+        this.login = this.login.bind(this);
+    }
+    async register() {
+        const { email, password, username } = this.state;
+        const registeredUser = await axios.post('/auth/register', {
+            email, 
+            username, 
+            password
+        });
+        this.props.setUser(registeredUser.data);
+    }
+
+    async login() {
+        const { email, password } = this.state;
+        const loggedInUser = await axios.post('/auth/login', {
+            email,  
+            password
+        });
+        this.props.setUser(loggedInUser.data);
+    }
+
+    render() {
+        const { email, username, password, register } = this.state;
+        return <div className="auth-container">
+                <form onSubmit={(e) => {
+                    // prevent default to stop form from refreshing
+                    e.preventDefault()
+                }}>
+                    {/* username input */}
+                    {register && (
+                    <div className='input-container'>
+                        <label>username</label>
+                        <input value={username} onChange={(e) => this.setState({
+                            username: e.target.value 
+                        }) 
+                    }
+                    />
+                    </div>
+                    )}
+                    {/* email input */}
+                    <div className='input-container'>
+                        <label>email</label>
+                        <input 
+                        type="email"
+                        value={email} 
+                        onChange={(e) => this.setState({
+                            email: e.target.value 
+                        }) 
+                    }
+                    />
+                    </div>
+                    {/* password */}
+                    <div className='input-container'>
+                        <label>password</label>
+                        <input 
+                        type="password"
+                        value={password} 
+                        onChange={(e) => this.setState({
+                            password: e.target.value 
+                        }) 
+                    }
+                    />
+                    </div>
+                    <button>Register</button>
+                </form>
+                <button onClick={() => this.setState({
+                    register: true
+                })}>
+                    Register
+                </button>
+                <button onClick={() => this.setState({
+                    register: false
+                })}>
+                    Login
+                </button>
+                </div>
+    }
+}
+
+function mapReduxStateToProps(reduxState) {
+    return reduxState
+}
+
+const mapDispatchToProps = {
+    setUser
+}
+
+const enhancedComponent = connect(mapReduxStateToProps, mapDispatchToProps)
+
+export default enhancedComponent(AuthComponent);
